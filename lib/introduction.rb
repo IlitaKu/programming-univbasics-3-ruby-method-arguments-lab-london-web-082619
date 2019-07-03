1db10c185c48d899c252f30c503659cc69badcec
Ilita Kunelyte <kunelyte.ilita90@gmail.com>
2019-07-03 12:55:27 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -1,3 +1,9 @@
 def introduction (name)
   puts "Hi my name is #{name}."
 end
+def introduction_with_language (name, language)
+  puts "Hi my name is #{name} and I am learning to program on #{language}."
+end
+def introduction_with_language_optional (name, language = "Ruby")
+  puts "Hi my name is #{name} and I am learning to program on #{language}."
+end
